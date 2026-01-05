b988adb0cb6bb1aa1d4c95089402a3d677fa7919
Chris Krause <chrskrs@gmail.com>
2026-01-05 11:33:43 +0200

    added Learner model

--- learners/models.py
+++ learners/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Learner(models.Model):
+    lastName = models.CharField(max_length=50)
+    firstNames = models.CharField(max_length=50)
+    grade = models.IntegerField(default=8)
+    group = models.CharField(max_length=1)
+    status = models.IntegerField(default=0)
+
+
+
