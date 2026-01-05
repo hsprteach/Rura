25698c3bafcad5b63521d2216eab224f6d3d4cad
Chris Krause <chrskrs@gmail.com>
2026-01-05 12:21:50 +0200

    show form and save data  43:00

--- learners/urls.py
+++ learners/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('new', views.home),
     path('func', views.learners_fn),
     path('cls', views.Learners_cls.as_view()), 
 ]
--- learners/views.py
+++ learners/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import LearnerDetail
 
 # Create your views here.
+
+def home(request):
+    form = LearnerDetail()
+    if request.method == 'POST':
+        form = LearnerDetail(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+        
+    return render(request, 'index.html', {'form' : form})    
+
 def learners_fn(request):
     return HttpResponse("<h1>Hi from func view</h1>")
 
